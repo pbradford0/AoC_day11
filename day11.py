d225b8a9f3d3eba9117240d2f0fd15f2dda0ea07
pbradford0 <phil.w.bradford@gmail.com>
2016-11-21 20:50:06 -0500

    break time

--- day11.py
+++ day11.py
@@ -7,38 +7,41 @@ import re
 def increment_1(input):
   password = ""
   new_pass = []
+  re_iol = re.compile("[iol]")
+  re_dubs = re.compile('.*(.)\\1.*(.)\\2.*')
   #reverse the password to do the maths
   password = input[::-1]
-  #convert old password into ints
-  for char in password:
-    new_pass.append(ord(char))
   #a = 97, z = 122
   #increment first char by 1. if z, set to a, then carry the 1
-  for x,char in enumerate(new_pass):
-    if char >= 97 and char < 122:
-      new_pass[x] += 1
-      break
-    new_pass[x] = 97
-  #deconvert new password from ints to chars and anti-reverse
-  password = ""
-  for char in reversed(new_pass):
+  while True:
+    #convert old password into ints
+    for char in password:
+      new_pass.append(ord(char))
+    for x,char in enumerate(new_pass):
+      if char >= 97 and char < 122:
+        new_pass[x] += 1
+        break
+      new_pass[x] = 97
+    #deconvert new password from ints to chars and anti-reverse
+    password = ""
+    for char in reversed(new_pass):
     password += chr(char)
+    if not re.match(re_iol, password):
+      if re.match(re_dubs, password):
+          break
+  
   return password
 
 def pass_change(filename):
   password = ""
-  re_iol = re.compile("[iol]")
-  re_dubs = re.compile('.*(.)\\1.*(.)\\2.*')
+  
   #return a lowercase 8char string
   input = open(filename, 'rU').read()
   print "Santa's old password is: " + input
   password = input
   while True:
     password = increment_1(password)
-    if not re.match(re_iol, password):
-      if re.match(re_dubs, password):
-        
-          break
+    
     
   #re-reverse it now that the maths are done
   #password = password[::-1]
